42851319b42a04c9845c324585a7295950f25252
Magnus <magnus@ambition.com>
2025-11-20 16:49:58 -0800

    fix: spelling and types

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ async function run(): Promise<void> {
   try {
     core.debug('INIT!')
     const token = core.getInput('token')
-    const channels = core.getInput('channels')
+    const channel = core.getInput('channel')
     const workdir = core.getInput('workdir') || 'cypress'
     const messageText =
       core.getInput('message-text') ||
       "A Cypress test just finished. I've placed the screenshots and videos in this thread. Good pie!"
 
     core.debug(`Token: ${token}`)
-    core.debug(`Channels: ${channels}`)
+    core.debug(`Channel: ${channel}`)
     core.debug(`Message text: ${messageText}`)
 
     core.debug('Initializing slack SDK')
@@ -38,13 +38,17 @@ async function run(): Promise<void> {
     core.debug('Sending initial slack message')
     const result = await slack.chat.postMessage({
       text: "I've got test results coming in from Cypress. Hold tight ...",
-      channel: channels
+      channel
     })
 
-    // TODO: Check why Slack doesn't have the expected results from their API
-    //       calls defined as the return type. Maybe a generic is needed?
-    const threadID = result.ts as string
-    const channelId = result.channel as string
+    const threadID = result.ts
+    const channelId = result.channel
+
+    if (!threadID || !channelId) {
+      core.error('No thread ID or channel ID returned from Slack. Exiting!')
+      core.setFailed('No thread ID or channel ID returned from Slack!')
+      return
+    }
 
     if (screenshots.length > 0) {
       core.debug('Uploading screenshots...')
@@ -53,7 +57,7 @@ async function run(): Promise<void> {
         screenshots.map(async screenshot => {
           core.debug(`Uploading ${screenshot}`)
 
-          await slack.files.upload({
+          await slack.files.uploadV2({
             filename: screenshot,
             file: createReadStream(`${workdir}/${screenshot}`),
             thread_ts: threadID,
@@ -72,7 +76,7 @@ async function run(): Promise<void> {
         videos.map(async video => {
           core.debug(`Uploading ${video}`)
 
-          await slack.files.upload({
+          await slack.files.uploadV2({
             filename: video,
             file: createReadStream(`${workdir}/${video}`),
             thread_ts: threadID,
