e7f13b7afc901cc8229bd6197963f0ed2ac8cd6b
Magnus <magnus@ambition.com>
2025-11-20 20:34:05 -0800

    fix: ci

---
+++ dist/main.js
@@ -0,0 +1,80 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const core = tslib_1.__importStar(require("@actions/core"));
+const fs_1 = require("fs");
+const walk_sync_1 = tslib_1.__importDefault(require("walk-sync"));
+const web_api_1 = require("@slack/web-api");
+async function run() {
+    try {
+        core.debug('INIT!');
+        const token = core.getInput('token');
+        const channel = core.getInput('channel');
+        const workdir = core.getInput('workdir') || 'cypress';
+        const messageText = core.getInput('message-text') ||
+            "A Cypress test just finished. I've placed the screenshots and videos in this thread. Good pie!";
+        core.debug(`Channel: ${channel}`);
+        core.debug(`Message text: ${messageText}`);
+        core.debug('Initializing slack SDK');
+        const slack = new web_api_1.WebClient(token);
+        core.debug('Slack SDK initialized successfully');
+        core.debug('Checking for videos and/or screenshots from cypress');
+        const videos = (0, walk_sync_1.default)(workdir, { globs: ['**/*.mp4'] });
+        const screenshots = (0, walk_sync_1.default)(workdir, { globs: ['**/*.png'] });
+        if (videos.length <= 0 && screenshots.length <= 0) {
+            core.debug('No videos or screenshots found. Exiting!');
+            core.setOutput('result', 'No videos or screenshots found!');
+            return;
+        }
+        core.debug(`Found ${videos.length} videos and ${screenshots.length} screenshots`);
+        core.debug('Sending initial slack message');
+        const result = await slack.chat.postMessage({
+            text: "I've got test results coming in from Cypress. Hold tight ...",
+            channel
+        });
+        const threadID = result.ts;
+        const channelId = result.channel;
+        if (!threadID || !channelId) {
+            core.error('No thread ID or channel ID returned from Slack. Exiting!');
+            core.setFailed('No thread ID or channel ID returned from Slack!');
+            return;
+        }
+        if (screenshots.length > 0) {
+            core.debug('Uploading screenshots...');
+            await Promise.all(screenshots.map(async (screenshot) => {
+                core.debug(`Uploading ${screenshot}`);
+                await slack.files.uploadV2({
+                    filename: screenshot,
+                    file: (0, fs_1.createReadStream)(`${workdir}/${screenshot}`),
+                    thread_ts: threadID,
+                    channel_id: channelId
+                });
+            }));
+            core.debug('...done!');
+        }
+        if (videos.length > 0) {
+            core.debug('Uploading videos...');
+            await Promise.all(videos.map(async (video) => {
+                core.debug(`Uploading ${video}`);
+                await slack.files.uploadV2({
+                    filename: video,
+                    file: (0, fs_1.createReadStream)(`${workdir}/${video}`),
+                    thread_ts: threadID,
+                    channel_id: channelId
+                });
+            }));
+            core.debug('...done!!');
+        }
+        core.debug('Updating message to indicate a successful upload');
+        await slack.chat.update({
+            ts: threadID,
+            channel: channelId,
+            text: messageText
+        });
+        core.setOutput('result', 'Bingo bango bongo!');
+    }
+    catch (error) {
+        core.setFailed(error instanceof Error ? error.message : String(error));
+    }
+}
+run();
+//# sourceMappingURL=main.js.map
